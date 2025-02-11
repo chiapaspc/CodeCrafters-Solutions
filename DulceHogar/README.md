e1ebc59d3723839d158e2eb8534f7e2edf0faa05
Alftredo Hernandez <chiapaspc@hotmail.com>
2025-02-11 16:05:51 -0600

    Se agrego en DulceHogar registro de ventas y reporte de ventas diarias.

--- DulceHogar/README.md
+++ DulceHogar/README.md
@@ -5,4 +5,11 @@ Este proyecto es un sistema básico para gestionar el inventario y las ventas de
 # Funcionalidades
 
 - Registrar productos.
+    -- Nombre, cantidad en stock y precio.
 - Mostrar inventario. 
+    -- Listar todos los productos con su información.
+- Registrar ventas.
+    -- Seleccionar un producto y registrar la cantidad vendida.
+    -- Actualizar el stock del producto.
+- Generar reporte de ventas.
+    -- Mostrar un resumen de las ventas del dia.
--- DulceHogar/main.py
+++ DulceHogar/main.py
@@ -1,6 +1,8 @@
 #Sistema de gestión de inventario y ventas para Dulce Hogar
 
 inventario = []
+ventas = []
+articulos_vendidos = []
 
 def registrar_producto():
     nombre = input("Nombre del producto: ")
@@ -10,16 +12,92 @@ def registrar_producto():
     inventario.append(producto)
     print("Producto registrado con éxito.")
 
+def nueva_venta():
+    nombre_cliente = input("Nombre del cliente: ")
+    total_productos = 0
+    total = 0.0
+    id_venta = len(ventas) + 1
+    venta = {"nombre_cliente": nombre_cliente, "total_productos": total_productos, "total": total, "id" : id_venta}
+    ventas.append(venta)
+    print("Venta creada: ", id_venta)
+    venta_producto(id_venta)
+
+def agregar_producto_venta(id_venta, producto, cantidad, precio):
+    nombre = producto ["nombre"]
+    id_venta = id_venta
+    cantidad = cantidad
+    precio = precio
+    subtotal = precio * cantidad
+    producto = {"id": id_venta, "cantidad": cantidad, "nombre": nombre, "precio": precio, "subtotal": subtotal}
+    articulos_vendidos.append(producto) 
+
+def totalizar_venta(id_venta):
+    id_venta = id_venta
+    articulos = 0
+    total = 0
+    for producto in articulos_vendidos:
+        if producto["id"] == id_venta:
+            articulos += producto["cantidad"]
+            total = total + producto['subtotal']
+            for venta in ventas:
+                if id_venta == venta["id"]:
+                    venta["total"] = total
+                    venta["total_productos"] = articulos
+    print("Venta finalizada total: ", total)
+    print("Numero de Articulos: ", articulos)
+
+    
+
+def venta_producto(id):
+    while True:
+        id_venta = id
+        print("\n-- Venta ", id_venta, "--")
+        print("1. Agregar Producto")
+        print("2. Cerrar Venta")
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            nombre = input("Nombre del producto: ")
+            encontrado = False
+            for producto in inventario:
+                if producto["nombre"] == nombre:
+                    encontrado = True
+                    print(f"Existencia: {producto['cantidad']}")
+                    cantidad = int(input("Cantidad a vender: "))
+                    if cantidad <= producto["cantidad"] :
+                            precio = producto["precio"]
+                            agregar_producto_venta(id_venta, producto, cantidad, precio)
+                            producto["cantidad"]-= cantidad
+                            print(f"Producto agregado correctamente. Stock restante: {producto['cantidad']}")
+                            print(articulos_vendidos)
+                            break
+                    else:
+                        print("No hay suficiente stock disponible.")
+            if not encontrado:
+                print("El producto no existe")
+        elif opcion == "2":
+            totalizar_venta(id_venta)
+            break
+
 def mostrar_inventario():
     print("\n-- Inventario --")
     for producto in inventario:
         print(f"Nombre: {producto['nombre']}, Cantidad: {producto['cantidad']}, Precio: {producto['precio']} ")
 
+def mostrar_ventas():
+    print("\n-- Ventas del dia --")
+    for venta in ventas:
+        print(f"Nombre de Cliente: {venta['nombre_cliente']}, Articulos: {venta['total_productos']}, Total Venta: ${venta['total']} ")
+
+
+
 while True:
     print("\n-- Menú --")
     print("1. Registrar producto")
     print("2. Mostrar inventario")
-    print("3. Salir")
+    print("3. Nueva Venta")
+    print("4. Reporte de Ventas")
+    print("5. Salir")
     opcion = input("Seleccione una opción: ")
 
     if opcion == "1":
@@ -27,6 +105,10 @@ while True:
     elif opcion == "2":
         mostrar_inventario()
     elif opcion == "3":
+        nueva_venta()
+    elif opcion == "4":
+        mostrar_ventas()
+    elif opcion == "5":
         break
     else:
         print("Opcion invalidad. Intente de nuevo.")
